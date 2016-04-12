e2262a70c98bcadd0ed80a1c44b403a6020418aa
bjonsson <benny.jonsson@big-social-media.com>
2016-04-12 16:45:49 +0200

    Now it's working!

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
-angular.module('app',['bigDropdownTree'])
+angular.module('appDropDown',['bigDropdownTree'])
 .controller('Controller', ['$scope', function($scope) {
-  $scope.bacon = "This is controller BACON";
-  $scope.baconFunction = function(key) {
-    alert("Bacon:" + key);  
+  
+  $scope.setUser = function(node,issueId) {
+    alert("From app" + node.key + " : " + node.value + " issueId: " + issueId );  
   };
-  $scope.numberTree =
-                 [{
-                        key: "1",
-                        value: "Ett"
-                    }, 
-                    {
-                        key: "2",
-                        value: "Två",
-                        nodes: [{
-                                key: "20",
-                                value: "Tjugo",
-                        },{key:"21",value:"tjugoett",
-                                nodes: [{key:"201",value:"Tvåhunraett"},
-                                {key:"202",value:"Två hundra två"}]
-                            }]
-                        }, 
-                        {
-                            key: "3",
-                            value: "Tre"
-                        }, {
-                            key: "4",
-                            value: "Fyra",
-                            nodes: [{ key: "4011", value: "Förtioelva" },
-                                { key: "4012", value: "Bacon ipsum dolor amet pork pork belly sausage ground round brisket ball tip landjaeger" }]
-                        }]; 
+  
+   $scope.iLikeBacon = function(ba,con) {
+    alert("I like " + ba.value + " age " + ba.age + " issueId: " + con );  
+  };
+  $scope.userTree =
+    [{key: "1",value: "Kalle"}, 
+    {key: "2", value: "Group 1",
+    nodes: [{key: "20",value: "Greger"},
+        {key:"21",value:"SubGroup 1",
+        nodes: [{key:"201",value:"Kent"},
+                {key:"202",value:"Jesus"}
+                ]
+            }]
+    },
+    {key: "3",value: "Tim"},
+    {key: "4",value: "Group 2",
+        nodes: [{ key: "4011", value: "Simon" },
+                { key: "4012", value: "Tiger" }]
+    }]; 
                         
-                         $scope.baconTree =
-                 [{
-                        key: "1",
-                        value: "Bacon 1"
-                    }, 
-                    {
-                        key: "2",
-                        value: "Bacon 2",
-                        nodes: [{
-                                key: "20",
-                                value: "Bacon 20",
-                        },{key:"21",value:"bacon 21",
-                                nodes: [{key:"201",value:"bacon 201"},
-                                {key:"202",value:"Bacon 202"}]
-                            }]
-                        }]
+$scope.baconTree =
+    [{key: "1",value: "Bacon 1",age:1}, 
+    {key: "2",value: "Bacon 2",age:12,
+        nodes: [{key: "20",value: "Bacon 20",age:42},
+                {key:"21",value:"bacon 21",age:99,
+                    nodes: [{key:"201",value:"bacon 201",age:69},
+                    {key:"202",value:"Bacon 202", age:666}]
+                }]
+    }];
+
+$scope.sentiment =
+    [
+        {key:null,value:'Do not set',class:'fa fa-minus-square-o'},
+        {key:'1',value:'Positive' ,class:'fa fa-plus-square'},
+        {key:'2',value:'Neutral' ,class:'fa fa-check-square'},
+        {key:'3',value:'Negative',class:'fa fa-minus-square' },
+    ];
 }]);
--- directives/bigDropdownTree.js
+++ directives/bigDropdownTree.js
@@ -2,23 +2,36 @@
     'use strict';
     angular
         .module('bigDropdownTree', [])
-        .directive('bigDropdownTree', function() {
+        .directive('bigDropdownTree',[ function() {
             return {
-                // restrict: 'E',
+                restrict: 'AE',
                 scope: {
-                    tree:'=ngModel',
-                    bigDropdownTree: '&'
+                    ngModel: '=',
+                    onNodeClick: '&',
                 },
                 templateUrl: 'directives/bigDropdownTree.tpl.html',
                 require: 'ngModel',
                 link: function(scope, element, attrs, ctrl) {
-                   scope.bigDropdownTree(
-                       {
-                           key:'Tralla'
-                       }
-                   );
+                    scope.id = attrs["issueid"];
+                    scope.openLabel = attrs["openlabel"] || "Click to open";
+                    scope.showDropdown = false;
+                    scope.showNodes = {};
+                    scope.nodeClicked = function(node) {
+                        console.log(node.value);
+                        scope.showDropdown = false;
+                        scope.onNodeClick()(node, scope.id);
+                    }
+                    scope.toggleDropdown = function() {
+                        scope.showDropdown = !scope.showDropdown;
+                    }
+                    scope.closeDropdown = function() {
+                        scope.showDropdown = false;
+                    }
+                    scope.toggleNodes = function(key) {
+                        scope.showNodes[key] = !scope.showNodes[key];
+                    }
                 }
             };
-        });
+        }]);
 
 })(angular);
